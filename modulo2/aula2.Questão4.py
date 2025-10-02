8185f066f8f202f910d87dc010f26ad0314ff40e
Nicssnts <nicoleteisis@gmail.com>
2025-10-02 18:53:59 -0300

    Add files via upload

---
+++ modulo2/aula2.Questão4.py
@@ -0,0 +1,12 @@
+# Saldo inicial e taxa de juros
+saldo = 500.0
+juros = 1.01
+
+# Calcular saldo após 3 meses usando apenas duas variáveis
+saldo = saldo * juros  # 1º mês
+saldo = saldo * juros  # 2º mês
+saldo = saldo * juros  # 3º mês
+
+# Imprimir o resultado
+print("Após 3 meses meu novo saldo é")
+print(saldo)
---
+++ modulo2/aula2_Questão2.py
@@ -0,0 +1,17 @@
+texto = "o resultado é"
+num1 = 10
+num2 = 3.5
+
+resultado = num1 + num2
+
+print(texto)
+print(type(texto))
+
+print(num1)
+print(type(num1))
+
+print(num2)
+print(type(num2))
+
+print(resultado)
+print(type(resultado))
